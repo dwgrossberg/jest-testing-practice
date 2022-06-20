748d70d7e8b829aa25507bd526f99626429b3e0d
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-20 22:54:47 +0700

    Finish capitalize.js and capitalize.test.js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,6 +1,8 @@
-const capitalize = (() => {
-  const value = "a";
-  return value;
-})();
+const capitalize = (abc) => {
+  return (
+    String(abc).toLowerCase().charAt(0).toUpperCase() +
+    String(abc).toLowerCase().substring(1)
+  );
+};
 
 export default capitalize;
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -7,5 +7,25 @@ it("happy path", () => {
 });
 
 it("string", () => {
-  expect(typeof capitalize).toBe("string");
+  expect(typeof capitalize()).toBe("string");
+});
+
+it("first letter capital", () => {
+  expect(capitalize().charAt(0)).toBe(capitalize().charAt(0).toUpperCase());
+});
+
+it("other letters not", () => {
+  expect(capitalize().slice(1)).toBe(capitalize().slice(1).toLowerCase());
+});
+
+it("test1", () => {
+  expect(capitalize("abcBBBddkeWWWekeeWff")).toBe("Abcbbbddkewwwekeewff");
+});
+
+it("test2", () => {
+  expect(capitalize("a")).toBe("A");
+});
+
+it("test3", () => {
+  expect(capitalize("AAAAAAAAAA")).toBe("Aaaaaaaaaa");
 });
