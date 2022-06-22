656615891f3eed7d98298c93ae09d1f8d49d9122
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-22 20:38:33 +0700

    Finish caesarCipher.js and tests

--- src/caeserCipher.js
+++ src/caeserCipher.js
@@ -1,9 +1,13 @@
-const caeserCipher = (num, string) => {
+const caeserCipher = (key, string) => {
   const re = /[a-zA-Z]/;
-  let cipher;
+  let cipher = "";
   for (let i = 0; i < string.length; i++) {
     if (re.test(string.charAt(i))) {
-      cipher += i;
+      cipher += String.fromCharCode(
+        ((string.charCodeAt(i) - 97 + key) % 26) + 97
+      );
+    } else {
+      cipher += string.charAt(i);
     }
   }
   return cipher;
--- tests/caeserCipher.test.js
+++ tests/caeserCipher.test.js
@@ -7,17 +7,35 @@ it("happy path", () => {
 });
 
 it("is string", () => {
-  expect(typeof caeserCipher()).toBe("string");
+  expect(typeof caeserCipher(1, "a")).toBe("string");
 });
 
 it("string is unchanged with a shift of 0", () => {
-  expect(caeserCipher(0, "abc")).toBe(caeserCipher("abc"));
+  expect(caeserCipher(0, "abc")).toBe("abc");
 });
 
 it("string is different with a shift of 1-25", () => {
-  expect(caeserCipher(1, "abc")).not.toBe(caeserCipher("abc"));
+  expect(caeserCipher(1, "abc")).not.toBe("abc");
 });
 
-it("translates one letter correctly", () => {
+it("encodes one letter correctly", () => {
   expect(caeserCipher(1, "a")).toBe("b");
+});
+
+it("encodes a sentence correctly", () => {
+  expect(caeserCipher(17, "a pig went to the bank to buy some gum")).toBe(
+    "r gzx nvek kf kyv sreb kf slp jfdv xld"
+  );
+});
+
+it("wraps around the alphabet from z to a", () => {
+  expect(caeserCipher(40, "test wrapping")).toBe("hsgh kfoddwbu");
+});
+
+it("wraps a perfect circle at multiples of 26", () => {
+  expect(caeserCipher(26, "perfect circle")).toBe("perfect circle");
+});
+
+it("preserves capital letters", () => {
+  expect(caeserCipher(6, "The Pig In The Barn")).toBe("Znk Vom Ot Znk Hgxt");
 });
